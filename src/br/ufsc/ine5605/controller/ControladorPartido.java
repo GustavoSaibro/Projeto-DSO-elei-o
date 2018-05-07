24cab2b7dff95a0c1697d6ed71a7f084c4a1ac26
45336757861 <45336757861@ine>
2018-05-07 19:47:38 -0300

    cadastro partido - McLovin

--- src/br/ufsc/ine5605/controller/ControladorPartido.java
+++ src/br/ufsc/ine5605/controller/ControladorPartido.java
@@ -3,9 +3,25 @@ package br.ufsc.ine5605.controller;
 import java.util.ArrayList;
 
 import br.ufsc.ine5605.model.Partido;
+import br.ufsc.ine5605.view.TelaPartido;
 
 public class ControladorPartido {
-	private ArrayList<Partido> partidos;
+    private ArrayList<Partido> partidos;
+    private Partido partido;
+    private TelaPartido telaPartido;
+
+    public void iniciaTelaPartido() {
+        telaPartido = new TelaPartido(this);
+        telaPartido.startPartido();
+    }
+
+    public void cadastrarPartido(String nomePartido, int numeroPartido) {
+        partido = new Partido();
+        partido.setNomePartido(nomePartido);
+        partido.setNumeroPartido(numeroPartido);
+        partidos = new ArrayList<>();
+        partidos.add(partido);
+    }
 	
 	
 	
