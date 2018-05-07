fd32982f8526e74590b6e83b19f17b0cb41fd984
pedro <pedro@200.135.76.189>
2018-05-07 19:48:27 -0300

    aloalo

---
+++ src/br/ufsc/ine5605/view/TelaPartido.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufsc.ine5605.view;
+
+/**
+ *
+ * @author pedro
+ */
+public class TelaPartido {
+    
+    public void opcoesPartido(){}
+    
+    public void cadastrarPartido(){}
+    
+    public void excluirPartido(){}
+    
+    public void listarPartido(){}
+    
+    
+}
