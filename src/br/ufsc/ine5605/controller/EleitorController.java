b08f8cb93223826c0a8c654890a64c838d6c9740
17204734 <17204734@ine>
2018-05-07 20:40:31 -0300

    GustavoCommit04

--- src/br/ufsc/ine5605/controller/EleitorController.java
+++ src/br/ufsc/ine5605/controller/EleitorController.java
@@ -13,12 +13,41 @@ private TelaEleitor telaEleitor;
 
 public void iniciaTelaEleitor(){
     this.telaEleitor = telaEleitor;
-    //telaEleitor.start();
+    //telaEleitor.opcoesEleitor();
 }
 
 public void cadastrarEleitor(int zonaEleitoral,Cidade nomeCidade,int titulo){
      eleitor = new Eleitor(zonaEleitoral,nomeCidade,titulo);
+     
+     /*
+     aqui faço uma isntancia de eleitor e percorro o array verificando se 
+     o mesmo já esta cadastrado.
+     */
+     
+     for(Eleitor e: eleitores){
+         if(!eleitor.equals(e)){
+             eleitores.add(eleitor);
+             break;
+         }
+     }
 }
 
+public ArrayList ListaEleitores(){
+    return eleitores;
+}
 
+public void excluirEleitorByTitulo(int titulo){
+    
+    /*
+    aqui verifico na lista de eleitores o tituo e se for igual ao passo de parametro eu excluo.
+    
+    */
+    
+    
+    for(Eleitor e : eleitores){
+        if(e != null && e.getTituloEleitor() == titulo){
+            e = null;
+        }
+    }
+}
 }
